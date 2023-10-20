c0dd3cd175d49bc3bf749bdc9c38405cf697f417
Philippe Laurens <philippe.laurens64@sfr.fr>
2023-10-20 15:34:32 +0000

    update version: 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup_args = dict(
     name='very_fantastic_ascii',
-    version='1.0.0',
+    version='1.0.2',
     description='Very Fantastic ASCII',
     license='MIT',
     packages=find_packages(),
